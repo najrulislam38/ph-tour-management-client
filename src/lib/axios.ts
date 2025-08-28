a0d8affc96da1412a0f82702d488c4c3dd2e748b
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-08-28 20:45:45 +0600

    Update axios interceptors for handling token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import config from "@/config";
-import axios from "axios";
+import axios, { type AxiosRequestConfig } from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: config.baseUrl,
@@ -28,35 +28,66 @@ axiosInstance.interceptors.request.use(
 
 let isRefreshing = false;
 
-const pendingQueue: {
-  resolve: (value, unknown) => void;
-  reject: (value, unknown) => void;
+let pendingQueue: {
+  resolve: (value: unknown) => void;
+  reject: (value: unknown) => void;
 }[] = [];
+
+const processQueue = (error: unknown) => {
+  pendingQueue.forEach((promise) => {
+    if (error) {
+      promise.reject(error);
+    } else {
+      promise.resolve(null);
+    }
+  });
+
+  pendingQueue = [];
+};
+
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
-    const originalRequest = error.config;
-
-    isRefreshing = true;
+    const originalRequest = error.config as AxiosRequestConfig & {
+      _retry: boolean;
+    };
 
     // console.log(originalRequest);
     if (
       error.response.status === 500 &&
-      error.response.data.message === "jwt expired"
+      error.response.data.message === "jwt expired" &&
+      !originalRequest._retry
     ) {
       console.log("Your token is expired");
 
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          pendingQueue.push({ resolve, reject });
+        })
+          .then(() => axiosInstance(originalRequest))
+          .catch((error) => Promise.reject(error));
+      }
+
+      isRefreshing = true;
       try {
         const res = await axiosInstance.post("/auth/refresh-token");
 
         console.log("New Token arrived", res);
 
+        processQueue(null);
+
         return axiosInstance(originalRequest);
       } catch (error) {
         console.error(error);
+        processQueue(error);
+        return Promise.reject(error);
+      } finally {
+        isRefreshing = false;
       }
     }
   }
